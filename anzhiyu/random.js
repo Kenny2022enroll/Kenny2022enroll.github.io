cbc5d1a44549581df8b58929bf672efc1b5fae02
Kenny <kenny2022enroll@126.com>
2025-02-12 22:46:08 +0800

    Site updated: 2025-02-12 22:46:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/12/2月总结（更新中）/","2025/02/12/我的原则/","2025/02/12/对高效提分的研究（更新中）/"];function toRandomPost(){
+var posts=["2025/02/12/我的原则/","2025/02/12/对高效提分的研究（更新中）/","2025/02/12/对改变拖延性格方法的研究（更新中）/","2025/02/12/2月记录（更新中）/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
