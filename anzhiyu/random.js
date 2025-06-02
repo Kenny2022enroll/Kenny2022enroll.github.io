647c58879eb3673e38123c331008be1301344b0d
Kenny <kenny2022enroll@126.com>
2025-06-02 23:01:41 +0800

    Site updated: 2025-06-02 23:01:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/25/博文栏目说明/"];function toRandomPost(){
+var posts=["2025/05/25/博文栏目说明/","2025/06/01/镜里镜外：自知与他知/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
