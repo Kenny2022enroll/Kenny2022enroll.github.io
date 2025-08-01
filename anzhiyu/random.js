546a9c04dc962463f5685d21f884f76fd39866c4
Kenny <kenny2022enroll@126.com>
2025-08-01 10:43:51 +0800

    Site updated: 2025-08-01 10:43:51

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2025/06/05/To Persist or to Give Up/","2025/06/01/镜里镜外：自知与他知/","2025/05/25/博文栏目说明/","2025/07/02/阿德勒解梦——《自卑与超越》中关于“梦”的解释/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
