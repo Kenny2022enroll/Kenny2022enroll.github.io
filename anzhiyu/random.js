1162dbd7bbec28c84fb9e5b1b972b2205fed0f4c
Kenny <kenny2022enroll@126.com>
2025-08-03 23:23:54 +0800

    Site updated: 2025-08-03 23:23:54

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2025/05/25/博文栏目说明/","2025/08/02/缺爱童年的阴影——《不原谅也没关系》读书笔记/","2025/06/01/镜里镜外：自知与他知/","2025/07/02/阿德勒解梦——《自卑与超越》中关于“梦”的解释/","2025/06/05/To Persist or to Give Up/","1970/01/01/进度板/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
