373ab39ff455c5b818be71c8c1aa30e1e9e75fab
Kenny <kenny2022enroll@126.com>
2025-08-06 16:28:59 +0800

    Site updated: 2025-08-06 16:28:58

--- js/scroll.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('scroll', () => {
-  const scrollY = window.scrollY;
-  document.querySelectorAll('[data-scroll]').forEach(el => {
-    const speed = parseFloat(el.dataset.scroll || 0.1);
-    el.style.transform = `translateY(${scrollY * speed}px)`;
-  });
-});
